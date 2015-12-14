1a0cef604ec07290efdf290143f9392b3bd1654f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-12-14 16:36:13 +0100

    Implement ensurable

--- lib/puppet/type/container.rb
+++ lib/puppet/type/container.rb
@@ -1,7 +1,29 @@
 Puppet::Type.newtype(:container) do
   @doc = "Manage a container."
 
-  ensurable
+  ensurable do
+    newvalue(:created, :event => :container_created) do
+      provider.create
+    end
+
+    newvalue(:restarting, :event => :container_restarted) do
+      provider.restart
+    end
+
+    newvalue(:running, :event => :container_running) do
+      provider.run
+    end
+
+    newvalue(:paused, :event => :container_paused) do
+      provider.pause
+    end
+
+    newvalue(:exited, :event => :container_exited) do
+      provider.exit
+    end
+
+    defaultto { :running }
+  end
 
   newparam(:name) do
     desc "The default namevar."
--- spec/unit/puppet/type/container_spec.rb
+++ spec/unit/puppet/type/container_spec.rb
@@ -22,6 +22,34 @@ describe Puppet::Type.type(:container) do
           end
         end
       end
+
+      describe 'when validating values' do
+        describe 'for ensure' do
+          it 'should support :created as a value to :ensure' do
+            expect { described_class.new(:name => 'foo', :ensure => :created) }.to_not raise_error
+          end
+
+          it 'should support :restarting as a value to :ensure' do
+            expect { described_class.new(:name => 'foo', :ensure => :restarting) }.to_not raise_error
+          end
+
+          it 'should support :running as a value to :ensure' do
+            expect { described_class.new(:name => 'foo', :ensure => :running) }.to_not raise_error
+          end
+
+          it 'should support :paused as a value to :ensure' do
+            expect { described_class.new(:name => 'foo', :ensure => :paused) }.to_not raise_error
+          end
+
+          it 'should support :exited as a value to :ensure' do
+            expect { described_class.new(:name => 'foo', :ensure => :exited) }.to_not raise_error
+          end
+
+          it 'should not support other values for :ensure' do
+            expect { described_class.new(:name => 'foo', :ensure => :blahblah) }.to raise_error Puppet::Error, /Invalid value/
+          end
+        end
+      end
     end
   end
 end
